b56157b092255cfb701256200dfd980f21d6d3d7
Ped_ <pfdsilva@gmail.com>
2020-08-06 14:20:37 +0100

    different mapping for resources test

--- src/main/java/org/academiadecodigo/hackathon/heHadItComing/controller/MainController.java
+++ src/main/java/org/academiadecodigo/hackathon/heHadItComing/controller/MainController.java
@@ -5,26 +5,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(path = "/")
 public class MainController {
 
     @RequestMapping(method = RequestMethod.GET, path = {"", "/"})
     public String showHomepage() {
-        return "/index.html";
+        return "templates/index.html";
     }
 
     @RequestMapping(method = RequestMethod.GET, path = {"/navbar.css"})
     public String navbarStyle() {
-        return "/navbar.css";
+        return "templates/navbar.css";
     }
 
     @RequestMapping(method = RequestMethod.GET, path = {"/homepage.css"})
     public String homepageStyle() {
-        return "/homepage.css";
+        return "templates/homepage.css";
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/shop.html")
     public String showShop() {
-        return "/shop.html";
+        return "templates/shop.html";
     }
 
 
